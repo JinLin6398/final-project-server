2337631b6efa5917993f293d09f99794bfc800c7
yaritzay27 <yaritzay27@LAPTOP-A2EPPN7G>
2024-12-09 10:42:31 -0500

    Backend: Campus & Student Model Complete

--- database/models/Student.js
+++ database/models/Student.js
@@ -15,6 +15,34 @@ const Student = db.define("student", {
   lastname: {
     type: Sequelize.STRING,
     allowNull: false
+  },
+
+  // Change Made : email - not allow null/empty
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true,
+    },
+  },
+
+  // Change Made : imageUrl - with a default value, allow null/empty
+  imageUrl: {
+    type: Sequelize.STRING,
+    allowNull: true, // Optional
+    defaultValue: 'https://t4.ftcdn.net/jpg/02/15/84/43/360_F_215844325_ttX9YiIIyeaR7Ne6EaLLjMAmy4GvPC69.jpg',
+  },
+
+  // Change Made : gpa - decimal between 0.0 and 4.0, allow null/empty
+  gpa: {
+    type: Sequelize.DECIMAL(2, 1),
+    allowNull: true,
+    validate: {
+      isDecimal: true,
+      min: 0.0,
+      max: 4.0,
+    },
   }
 });
 
